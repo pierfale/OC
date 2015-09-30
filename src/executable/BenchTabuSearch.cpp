927c85d7b6bfcb51aaac1835637dc30b39bad31f
Pierre Falez <pierre.falez@gmail.com>
2015-09-30 14:03:34 +0200

    Add tabu search bench

---
+++ src/executable/BenchTabuSearch.cpp
@@ -0,0 +1,35 @@
+#include "solver/TabuSearchSolver.h"
+#include "shared/Benchmark.h"
+
+
+int main(int argc, const char** argv) {
+	if(argc != 5) {
+		std::cout << "usage : <problem_size> <input_file> <input_max_score> <output_file>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+
+	Benchmark<30> bench;
+
+	std::vector<const char*> inits = {"mdd"};
+
+#define ARGS_NUMBER 5
+
+	const char* bench_argv[ARGS_NUMBER];
+	bench_argv[0] = argv[0];
+	bench_argv[1] = argv[1];
+	bench_argv[2] = argv[2];
+	bench_argv[3] = "-init";
+
+	for(unsigned int init_index = 0; init_index < inits.size(); init_index++) {
+		bench_argv[4] = inits[init_index];
+
+		std::string name(bench_argv[4]);
+
+		bench.execute<TabuSearchSolver, TabuSearchProgramOption>(ARGS_NUMBER, bench_argv, argv[3], name);
+	}
+
+	bench.save(argv[4]);
+
+	return EXIT_SUCCESS;
+}
